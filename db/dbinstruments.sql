fe4218bd7d3f5e978fcd679e9d0a6279b53e3cc3
saad <saad.moumou.coder@gmail.com>
2022-11-13 22:10:00 +0100

    add db

--- db/dbinstruments.sql
+++ db/dbinstruments.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 12, 2022 at 01:26 PM
+-- Generation Time: Nov 13, 2022 at 09:09 PM
 -- Server version: 8.0.27
 -- PHP Version: 7.4.26
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `instruments`
+-- Database: `dbinstruments`
 --
 
 -- --------------------------------------------------------
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `fammiles` (
 
 INSERT INTO `fammiles` (`id`, `name`) VALUES
 (1, 'Bois'),
-(2, '\r\nClaviers\r\n'),
+(2, 'Clavirs'),
 (3, 'Cordes\r\n'),
 (4, 'Cuivres\r\n'),
 (5, 'Percussions\r\n');
@@ -59,11 +59,23 @@ CREATE TABLE IF NOT EXISTS `instruments` (
   `description` text NOT NULL,
   `date` date NOT NULL,
   `qnt` int NOT NULL,
-  `price` decimal(10,0) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
   `fammille_id` int NOT NULL,
   PRIMARY KEY (`id`),
   KEY `instruments_ibfk_1` (`fammille_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=26 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `instruments`
+--
+
+INSERT INTO `instruments` (`id`, `name`, `img`, `description`, `date`, `qnt`, `price`, `fammille_id`) VALUES
+(19, 'dsa', 'WhatsApp Image 2022-11-09 at 19.43.33.jpg', 'dsa', '2022-11-03', 4, '23.00', 1),
+(20, 'piano', '1665422549484__DSC0078[1].jpg', 'dsa', '2022-11-08', 2, '23.00', 1),
+(21, 'dsa', 'icons8-javascript-48.png', 'dsa', '2022-11-01', 2, '3.00', 2),
+(22, 'dsa', 'icons8-javascript-48.png', 'dsadsadsadsad', '2022-11-05', 23, '2.00', 4),
+(24, 'dsad', 'cover-scrum-board.png', 'dsa', '2022-11-08', 3, '2.00', 1),
+(25, 'beby', 'WhatsApp Image 2022-11-09 at 19.43.33.jpg', 'dsadsa', '2022-11-10', 24, '42.00', 1);
 
 -- --------------------------------------------------------
 
@@ -79,7 +91,7 @@ CREATE TABLE IF NOT EXISTS `shows` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   KEY `instrument_id` (`instrument_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
 
